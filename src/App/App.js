bdce98504d991215a6a4342304bfa82a46eb6eb4
Terrence L. Jones <terrenceljones805@gmail.com>
2018-06-04 22:02:24 -0700

    Init project

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = function App() {
+    return (
+        <div className="App">
+            Git Gist Viewer main App
+      </div>
+    );
+}
+
+export default App;
